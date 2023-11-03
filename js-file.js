61071f230fc19d59d35c34babbb5a095cb2bdfa6
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-11-03 17:41:06 +0100

    added text in red  with JS (first exercise)

--- js-file.js
+++ js-file.js
@@ -1,1 +1,7 @@
-console.log("test")
+const container = document.querySelector('#container');
+
+const p = document.createElement('p');
+    p.textContent = "Hey I´m Red!";
+    p.style.color = "#d40614";
+
+container.appendChild(p);
