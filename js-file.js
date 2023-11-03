03e037290adf13a3a99ce514ce1b20ec67858040
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-11-03 17:51:11 +0100

    added h3 in blue with JS (second exercise)

--- js-file.js
+++ js-file.js
@@ -1,7 +1,11 @@
 const container = document.querySelector('#container');
 
-const p = document.createElement('p');
+const p = document.createElement("p");
     p.textContent = "Hey I´m Red!";
     p.style.color = "#d40614";
 
-container.appendChild(p);
+const h3 = document.createElement("h3");
+    h3.textContent ="I´m a blue h3!";
+    h3.style.color = "#0a06d4";
+
+container.appendChild(p).appendChild(h3);
